d63cfa7be584693984aaf90e6010b590bd0182a9
Jacob Gonzales <35240561+JacobGonzales@users.noreply.github.com>
2020-04-16 21:13:02 -0600

    Updating Html In Javascript

--- Project3/wwwroot/js/site.js
+++ Project3/wwwroot/js/site.js
@@ -19,3 +19,22 @@ connection.on("writeToPage", function (messages) {
 });
 
 var array = ["one", "two", "three"]
+
+function createNewGroup(){
+
+    
+
+	var divTag = document.createElement("div");               //Create Tags needed for new group
+	var pTag = document.createElement("p");
+	var h3Tag = document.createElement("h3");
+
+	var h3Text = document.createTextNode("New Group One");    //Create text for each tag created
+	var pText = document.createTextNode("This is the new group that was made from the div groupOne.");
+
+	h3Tag.appendChild(h3Text);                                //Create text and tag together
+	pTag.appendChild(pText);
+    
+	var element = document.getElementById("groupOne");        //Get id for the div going into
+	element.appendChild(h3Tag);
+	element.appendChild(pTag);
+}
